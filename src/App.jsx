3c4f3d16796412ebffc9b43e384adc6bd90f7521
nashedshahroni22 <nashed15-11170@diu.edu.bd>
2023-09-13 18:32:41 +0600

    Working on Ball and Box

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Three from "./components/three/three";
 
 function App() {
   return (
-    <section className="md:flex p-5">
+    <section className=" p-5">
       <div className="md:w-1/2">
         <h1 className="text-3xl font-bold">Explore Three Fiber</h1>
       </div>
-      <div className="md:w-1/2">
-        <Canvas className="!h-[60vh] bg-slate-300"> 
+      <div className="">
+        <Canvas className="!h-[60vh]"> 
           <Suspense fallback={null}>
             <Three />
           </Suspense>
--- src/components/three/Three.jsx
+++ src/components/three/Three.jsx
@@ -1,9 +1,24 @@
+import { PerspectiveCamera } from "@react-three/drei";
 import React from "react";
 
 export default function Three() {
+  const angleToRadius = (angleInDeg) => (Math.PI / 100) * angleInDeg;
   return (
     <>
-      <ambientLight args={["#ffffff", 1]} />
+      <PerspectiveCamera makeDefault position={[0 , 1 ,5]}/>
+      {/* ball */}
+      <mesh position={[0,0.5,0]}>
+        <sphereGeometry args={[0.5, 32, 32]} />
+        <meshStandardMaterial color="#FFFFFF" />
+      </mesh>
+
+      {/* floor  */}
+      <mesh rotation={[-(angleToRadius(90)), 0, 0]}>
+        <planeGeometry args={[7, 7]} />
+        <meshStandardMaterial color="#lea3d8" />
+      </mesh>
+      {/* light */}
+      <ambientLight args={["#FFFFFF", 1]} />
     </>
   );
 }
