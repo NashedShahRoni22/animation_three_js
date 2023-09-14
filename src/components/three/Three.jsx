937c3468690f026fadc80b8137d3ffd4082a7b81
nashedshahroni22 <nashed15-11170@diu.edu.bd>
2023-09-14 17:58:32 +0600

    Anim Updating

--- src/components/three/Three.jsx
+++ src/components/three/Three.jsx
@@ -1,21 +1,32 @@
-import { PerspectiveCamera } from "@react-three/drei";
-import React from "react";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React, { useEffect, useRef } from "react";
 
 export default function Three() {
-  const angleToRadius = (angleInDeg) => (Math.PI / 100) * angleInDeg;
+  const angleToRadians = (angleInDeg) => (Math.PI / 100) * angleInDeg;
+  const orbitControlsRef = useRef(null);
+  useFrame((state) => {
+    // console.log(state.mouse);
+  });
+  useEffect(() => {
+    if (!!orbitControlsRef.current) {
+      console.log(orbitControlsRef.current);
+    }
+  }, [orbitControlsRef.current]);
   return (
     <>
-      <PerspectiveCamera makeDefault position={[0 , 1 ,5]}/>
+      <PerspectiveCamera makeDefault position={[0, 1, 5]} />
+      <OrbitControls ref={orbitControlsRef} />
       {/* ball */}
-      <mesh position={[0,0.5,0]}>
+      <mesh position={[0, 0.5, 0]}>
         <sphereGeometry args={[0.5, 32, 32]} />
         <meshStandardMaterial color="#FFFFFF" />
       </mesh>
 
       {/* floor  */}
-      <mesh rotation={[-(angleToRadius(90)), 0, 0]}>
-        <planeGeometry args={[7, 7]} />
-        <meshStandardMaterial color="#lea3d8" />
+      <mesh rotation={[-angleToRadians(90), 0, 0]}>
+        <planeGeometry args={[4, 4]} />
+        <meshStandardMaterial color="#ADD8E6" />
       </mesh>
       {/* light */}
       <ambientLight args={["#FFFFFF", 1]} />
